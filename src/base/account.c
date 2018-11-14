feb0fca351672b3efdf7c359aaef8d971b79c35c
codylico <svgmovement@gmail.com>
2018-11-14 01:42:13 -0500

    Add basic account structure
    This commit adds the account API, without any table of numbers.

---
+++ src/base/account.c
@@ -0,0 +1,136 @@
+
+#include "ledger.h"
+#include "util.h"
+
+/*
+ * Actualization of the account structure
+ */
+struct ledger_account {
+  unsigned char *name;
+  unsigned char *description;
+  int item_id;
+};
+
+/*
+ * Initialize an account.
+ * - a account to initialize
+ * @return one on success, zero on failure
+ */
+static int ledger_account_init(struct ledger_account* a);
+
+/*
+ * Clear out an account.
+ * - a account to clear
+ */
+static void ledger_account_clear(struct ledger_account* a);
+
+
+/* BEGIN static implementation */
+
+int ledger_account_init(struct ledger_account* a){
+  a->description = NULL;
+  a->name = NULL;
+  a->item_id = -1;
+  return 1;
+}
+
+void ledger_account_clear(struct ledger_account* a){
+  ledger_util_free(a->description);
+  a->description = NULL;
+  ledger_util_free(a->name);
+  a->description = NULL;
+  a->item_id = -1;
+  return;
+}
+
+/* END   static implementation */
+
+/* BEGIN implementation */
+
+struct ledger_account* ledger_account_new(void){
+  struct ledger_account* a = (struct ledger_account* )ledger_util_malloc
+    (sizeof(struct ledger_account));
+  if (a != NULL){
+    if (!ledger_account_init(a)){
+      ledger_util_free(a);
+      a = NULL;
+    }
+  }
+  return a;
+}
+
+void ledger_account_free(struct ledger_account* a){
+  if (a != NULL){
+    ledger_account_clear(a);
+    ledger_util_free(a);
+  }
+}
+
+unsigned char const* ledger_account_get_description
+  (struct ledger_account const* a)
+{
+  return a->description;
+}
+
+int ledger_account_set_description
+  (struct ledger_account* a, unsigned char const* desc)
+{
+  int ok;
+  unsigned char* new_desc = ledger_util_ustrdup(desc,&ok);
+  if (ok){
+    ledger_util_free(a->description);
+    a->description = new_desc;
+    return 1;
+  } else return 0;
+}
+
+unsigned char const* ledger_account_get_name
+  (struct ledger_account const* a)
+{
+  return a->name;
+}
+
+int ledger_account_set_name
+  (struct ledger_account* a, unsigned char const* desc)
+{
+  int ok;
+  unsigned char* new_desc = ledger_util_ustrdup(desc,&ok);
+  if (ok){
+    ledger_util_free(a->name);
+    a->name = new_desc;
+    return 1;
+  } else return 0;
+}
+
+int ledger_account_get_id(struct ledger_account const* a){
+  return a->item_id;
+}
+
+void ledger_account_set_id(struct ledger_account* a, int item_id){
+  if (item_id < 0){
+    a->item_id = -1;
+  } else {
+    a->item_id = item_id;
+  }
+  return;
+}
+
+int ledger_account_is_equal
+  (struct ledger_account const* a, struct ledger_account const* b)
+{
+  /* trivial accounts */
+  if (a == NULL && b == NULL) return 1;
+  else if (a == NULL || b == NULL) return 0;
+  /* compare top-level features */{
+    if (a->item_id != b->item_id)
+      return 0;
+    if (ledger_util_ustrcmp(a->name, b->name) != 0)
+      return 0;
+    if (ledger_util_ustrcmp(a->description, b->description) != 0)
+      return 0;
+  }
+  return 1;
+}
+
+
+/* END   implementation */
---
+++ src/base/account.h
@@ -0,0 +1,91 @@
+/*
+ * file: base/account.h
+ * brief: Account structure
+ * author: Cody Licorish (svgmovement@gmail.com)
+ */
+#ifndef __Ledger_base_account_H__
+#define __Ledger_base_account_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif /*__cplusplus*/
+
+/*
+ * brief: Account
+ */
+struct ledger_account;
+
+/*
+ * Construct a new account.
+ * @return the account on success, otherwise NULL
+ */
+struct ledger_account* ledger_account_new(void);
+
+/*
+ * Destroy an account.
+ * - a the account to destroy
+ */
+void ledger_account_free(struct ledger_account* a);
+
+/*
+ * Query the description of an account.
+ * - a account to query
+ * @return the description if available, otherwise NULL
+ */
+unsigned char const* ledger_account_get_description
+  (struct ledger_account const* a);
+
+/*
+ * Modify the description of an account.
+ * - a account to query
+ * - desc new description
+ * @return one on success, zero otherwise
+ */
+int ledger_account_set_description
+  (struct ledger_account* a, unsigned char const* desc);
+
+/*
+ * Query the name of an account.
+ * - a account to query
+ * @return the name if available, otherwise NULL
+ */
+unsigned char const* ledger_account_get_name
+  (struct ledger_account const* a);
+
+/*
+ * Modify the name of an account.
+ * - a account to query
+ * - name new name
+ * @return one on success, zero otherwise
+ */
+int ledger_account_set_name
+  (struct ledger_account* a, unsigned char const* name);
+
+/*
+ * Query the identifier of an account.
+ * - a account to query
+ * @return the identifier if available, otherwise -1
+ */
+int ledger_account_get_id(struct ledger_account const* a);
+
+/*
+ * Modify the identifier of an account.
+ * - a account to query
+ * - item_id new identifier (non-negative)
+ */
+void ledger_account_set_id(struct ledger_account* a, int item_id);
+
+/*
+ * Compare two accounts for equality.
+ * - a an account
+ * - b another account
+ * @return one if the accounts are equal, zero otherwise
+ */
+int ledger_account_is_equal
+  (struct ledger_account const* a, struct ledger_account const* b);
+
+#ifdef __cplusplus
+};
+#endif /*__cplusplus*/
+
+#endif /*__Ledger_base_account_H__*/
