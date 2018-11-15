a2c35bf54e3f8d53da96eb6e1ce790c9a6a4efa8
codylico <svgmovement@gmail.com>
2018-11-15 18:09:46 -0500

    Add table transport API
    This commit adds the functions for serializing a data table to and from
    CSV texts.
    
    Depends on 5d47df16df98ea8d9872aec93cc789b5099621de.
    Depends on ee6d7d249a7308e7a65e7b51a245050ea39f3139.

---
+++ src/io/table.h
@@ -0,0 +1,33 @@
+/*
+ * file: io/table.h
+ * brief: Table transport API
+ * author: Cody Licorish (svgmovement@gmail.com)
+ */
+#ifndef __Ledger_IO_table_H__
+#define __Ledger_IO_table_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif /*__cplusplus*/
+
+struct ledger_table;
+
+/*
+ * Parse a CSV text. The text might be clobbered after parsing.
+ * - table table to read into
+ * - csv csv text
+ * @return one on success, zero otherwise
+ */
+int ledger_io_table_parse_csv(struct ledger_table* table, unsigned char *csv);
+/*
+ * Print a CSV text.
+ * - table table to write out
+ * @return the text allocated on success, NULL otherwise
+ */
+unsigned char* ledger_io_table_print_csv(struct ledger_table const* table);
+
+#ifdef __cplusplus
+};
+#endif /*__cplusplus*/
+
+#endif /*__Ledger_IO_table_H__*/
