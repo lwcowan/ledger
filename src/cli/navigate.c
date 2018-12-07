a95708895dd7310eb1a08a8f28d57907e5991e47
Cody Licorish <svgmovement@gmail.com>
2018-12-07 18:36:27 -0500

    Refactor transaction line printing from navigate
    This commit separates the source handling transaction line printing from
    the source for book navigation. Transaction line formatting will be used
    by other command-line interface commands.

--- src/cli/navigate.c
+++ src/cli/navigate.c
@@ -10,6 +10,7 @@
 #include "../base/bignum.h"
 #include "../base/find.h"
 #include "line.h"
+#include "print.h"
 #include <stdio.h>
 
 
@@ -269,10 +270,6 @@ int ledger_cli_list(struct ledger_cli_line *tracking, int argc, char **argv){
           ledger_table_count_rows(ledger_account_get_table_c(account)));
       /* list transaction lines */{
         struct ledger_table_mark *mark, *end;
-        unsigned char path_text[60];
-        unsigned char amount_text[16];
-        unsigned char check_text[16];
-        unsigned char date_text[24];
         int line_count = 0;
         /* get table */{
           struct ledger_table const *table =
@@ -283,55 +280,8 @@ int ledger_cli_list(struct ledger_cli_line *tracking, int argc, char **argv){
         if (mark != NULL && end != NULL){
           result = 1;
           while (!ledger_table_mark_is_equal(mark,end)){
-            int row_journal_id, row_entry_id;
-            if (!ledger_table_fetch_id(mark,0,&row_entry_id)){
-              result = 0;
-              break;
-            }
-            struct ledger_act_path display_path;
+            ledger_cli_print_account_line(tracking, mark, stderr);
             line_count += 1;
-            if (!ledger_table_fetch_id(mark,0,&row_journal_id)){
-              result = 0;
-              break;
-            }
-            if (!ledger_table_fetch_id(mark,1,&row_entry_id)){
-              result = 0;
-              break;
-            }
-            display_path.path[0] =
-              ledger_find_journal_by_id(tracking->book, row_journal_id);
-            if (display_path.path[0] >= 0){
-              struct ledger_journal const* row_journal =
-                ledger_book_get_journal(tracking->book, display_path.path[0]);
-              display_path.path[1] =
-                ledger_find_entry_by_id(row_journal, row_entry_id);
-            } else display_path.path[1] = -1;
-            display_path.typ = LEDGER_ACT_PATH_ENTRY;
-            display_path.len = 2;
-            if (ledger_act_path_render
-                ( path_text, sizeof(path_text), display_path, tracking->book)
-                < 0)
-              break;
-            if (ledger_table_fetch_string
-                (mark,2,amount_text,sizeof(amount_text)) < 0)
-            {
-              result = 0;
-              break;
-            }
-            if (ledger_table_fetch_string
-                (mark,3,check_text,sizeof(check_text)) < 0)
-            {
-              result = 0;
-              break;
-            }
-            if (ledger_table_fetch_string
-                (mark,4,date_text,sizeof(date_text)) < 0)
-            {
-              result = 0;
-              break;
-            }
-            fprintf(stderr,"  %-60s\n    %16s %24s %16s\n",
-                path_text, amount_text, date_text, check_text);
             ledger_table_mark_move(mark, +1);
           }
           if (result == 0){
